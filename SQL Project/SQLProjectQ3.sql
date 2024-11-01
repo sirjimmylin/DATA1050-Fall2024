fd6f4eda3056541faca815848c39e88d4274bd68
Jimmy Lin <sirjimmylin@duck.com>
2024-11-01 15:10:09 -0400

    11/1 office hours commit

--- SQL Project/SQLProjectQ3.sql
+++ SQL Project/SQLProjectQ3.sql
@@ -1,14 +1,12 @@
-SELECT physicians.ssn
--- , COUNT(distinct drugs.name) AS prescribed_drugs
+SELECT 
+    physicians.ssn,
+    COUNT(DISTINCT prescriptions.drug_name) AS prescribed_drugs_count
 FROM physicians
-INNER JOIN prescriptions
-ON prescriptions.physician_id = physicians.ssn
-INNER JOIN drugs
-ON drugs.name = prescriptions.drug_name
-INNER JOIN contracts
-ON contracts.drug_name = drugs.name
-INNER JOIN companies
-ON companies.id = contracts.company_id
+INNER JOIN prescriptions ON prescriptions.physician_id = physicians.ssn
+INNER JOIN drugs ON drugs.name = prescriptions.drug_name
+INNER JOIN contracts ON contracts.drug_name = drugs.name
+INNER JOIN companies ON companies.id = contracts.company_id
 WHERE companies.name = 'DRUGXO'
--- GROUP BY physicians.ssn
--- ORDER BY prescribed_drugs;
+GROUP BY physicians.ssn
+ORDER BY prescribed_drugs_count DESC
+LIMIT 1;
